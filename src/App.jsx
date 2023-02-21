9122acec72f6782f9458c07ac405da1b64f4695e
VineethChandha <69765021+VineethChandha@users.noreply.github.com>
2023-02-20 23:12:30 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+/*import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App */
+
+import './App.css';
+import Calendar from './components/calender'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 days in Los Angeles  🌆</h1>
+      <h2>Welcome to Los Angeles, Abdul! Check out this calandar to get to know the city and see all the sights during your stay.</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
